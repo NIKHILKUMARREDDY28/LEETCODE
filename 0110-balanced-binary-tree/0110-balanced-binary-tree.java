93dd29a6d3bd858432263bf36e6562716505f90c
NIKHILKUMARREDDY28 <98465889+NIKHILKUMARREDDY28@users.noreply.github.com>
2022-10-12 12:26:21 +0530

    Time: 1 ms (99.17%), Space: 44.4 MB (39.51%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.java
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.java
@@ -15,9 +15,7 @@
  */
 class Solution {
     public boolean isBalanced(TreeNode root) {
-        if(root == null){
-            return true;
-        }
+      
         return helper(root) != -1;
     }
     private int helper(TreeNode root){
