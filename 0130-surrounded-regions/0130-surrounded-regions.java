eb332ce2abc77c04e281c80a8ce949b71e06b7e5
NIKHILKUMARREDDY28 <98465889+NIKHILKUMARREDDY28@users.noreply.github.com>
2022-12-23 11:57:26 +0530

    Time: 9 ms (40.69%), Space: 51.4 MB (57.77%) - LeetHub

---
+++ 0130-surrounded-regions/0130-surrounded-regions.java
@@ -0,0 +1,37 @@
+class Solution {
+    private void dfs(int row,int col,boolean[][] vis,char[][] board){
+        int n = vis.length;
+        int m = vis[0].length;
+        
+        if(row >= 0 && row < n && col >= 0 && col < m && board[row][col] == 'O'){
+            if(vis[row][col] != true){
+                vis[row][col] = true;
+                dfs(row+1,col,vis,board);
+                dfs(row-1,col,vis,board);
+                dfs(row,col+1,vis,board);
+                dfs(row,col-1,vis,board);
+            }
+          
+        }
+    }
+    public void solve(char[][] board) {
+        int n = board.length;
+        int m = board[0].length;
+        Queue<int[]> q = new LinkedList<>();
+        boolean canEsc[][] = new boolean[n][m];
+        for(int i=0;i<n;i++){
+            if(board[i][0] == 'O')dfs(i,0,canEsc,board);
+            if(board[i][m-1] == 'O')dfs(i,m-1,canEsc,board);
+        }
+        for(int i=0;i<m;i++){
+            if(board[0][i] == 'O')dfs(0,i,canEsc,board);
+            if(board[n-1][i] == 'O')dfs(n-1,i,canEsc,board);
+        }
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(canEsc[i][j] == false && board[i][j] == 'O')board[i][j]='X';
+            }
+        }
+        
+    }
+}
