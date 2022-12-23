7729bf8aade3402eab5a56d2b755d7deb2fdb90a
NIKHILKUMARREDDY28 <98465889+NIKHILKUMARREDDY28@users.noreply.github.com>
2022-12-23 08:03:00 +0530

    Time: 21 ms (67.76%), Space: 46 MB (83.89%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.java
+++ 0542-01-matrix/0542-01-matrix.java
@@ -1,22 +1,15 @@
-class Pair{
-    int x;
-    int y;
-    public Pair(int x,int y){
-        this.x = x;
-        this.y = y;
-    }
-}
+
 class Solution {
     public int[][] updateMatrix(int[][] mat) {
         int m = mat.length;
         int n = mat[0].length;
         int[][] ans = new int[m][n];
-        Queue<Pair> q = new LinkedList<>();
+        Queue<int[]> q = new LinkedList<>();
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
                 if(mat[i][j] == 0){
-                    q.add(new Pair(i,j));
-                    ans[i][j] = 0;
+                    q.add(new int[]{i,j});
+                    
                 } else {
                     ans[i][j] = -1;
                 }
@@ -28,8 +21,9 @@ class Solution {
             ++ dist;
             int size = q.size();
             for(int i=0;i<size;i++){
-                int row = q.peek().x;
-                int col = q.peek().y;
+                int roco[] = q.peek();
+                int row = roco[0];
+                int col = roco[1];
                 q.remove();
                 int[][] trav = {{0,-1},{0,1},{-1,0},{1,0}};
                 for(int[] cur:trav){
@@ -37,7 +31,7 @@ class Solution {
                     int c = col + cur[1];
                     if(r >= 0 && r < m && c >= 0 && c < n && ans[r][c] == -1) {
                         ans[r][c] = dist;
-                        q.add(new Pair(r,c));
+                        q.add(new int[]{r,c});
                     }
                 }
             }
