1e32f592621a4e03e975f1457d6420cbc11bddf4
NIKHILKUMARREDDY28 <98465889+NIKHILKUMARREDDY28@users.noreply.github.com>
2022-12-23 12:06:58 +0530

    Time: 2 ms (86.74%), Space: 52.3 MB (18.89%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.java
+++ 0130-surrounded-regions/0130-surrounded-regions.java
@@ -17,7 +17,7 @@ class Solution {
     public void solve(char[][] board) {
         int n = board.length;
         int m = board[0].length;
-        Queue<int[]> q = new LinkedList<>();
+       
         boolean canEsc[][] = new boolean[n][m];
         for(int i=0;i<n;i++){
             if(board[i][0] == 'O')dfs(i,0,canEsc,board);
