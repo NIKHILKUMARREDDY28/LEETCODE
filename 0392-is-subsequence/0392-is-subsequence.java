32f9ab0d9bea81b34833a1bbd86e23d5dd1604e1
NIKHILKUMARREDDY28 <98465889+NIKHILKUMARREDDY28@users.noreply.github.com>
2023-01-31 21:51:12 +0530

    Time: 3 ms (24.31%), Space: 44 MB (5.28%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.java
+++ 0392-is-subsequence/0392-is-subsequence.java
@@ -1,18 +1,28 @@
 class Solution {
-    private boolean my_fun(String s1,int idx1,String s2,int idx2){
+    private int my_fun(String s1,int idx1,String s2,int idx2,int[][] dp){
         if(idx2 >= s2.length()){
-            if(idx1 >= s1.length()) return true;
-            return false;
+            if(idx1 >= s1.length()) return 1;
+            return 0;
         }
-        if(idx1 >= s1.length()) return true;
+        if(idx1 >= s1.length()) return 1;
+        if(dp[idx1][idx2] != -1) return dp[idx1][idx2];
         if(s1.charAt(idx1) == s2.charAt(idx2)){
-            return my_fun(s1,idx1 +1 , s2,idx2+1);
+            dp[idx1][idx2] = my_fun(s1,idx1 +1 , s2,idx2+1,dp);
         }else{
-            return my_fun(s1,idx1, s2,idx2+1);
+            dp[idx1][idx2] = my_fun(s1,idx1, s2,idx2+1,dp);
         }
+        return dp[idx1][idx2];
     }
     public boolean isSubsequence(String s, String t) {
         if (s.isEmpty()) return true;
-        return my_fun(s,0,t,0);
+        int n = s.length();
+        int m = t.length();
+        int[][] dp = new int[n][m];
+            for(int i=0;i<n;i++){
+                for(int j=0;j<m;j++){
+                    dp[i][j] = -1;
+                }
+            }
+        return my_fun(s,0,t,0,dp) == 1;
     }
 }
