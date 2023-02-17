60330842812fdb204c30f5792c30b89a096a4329
NIKHILKUMARREDDY28 <98465889+NIKHILKUMARREDDY28@users.noreply.github.com>
2023-02-17 22:09:41 +0530

    Time: 26 ms (69.04%), Space: 44.2 MB (36.69%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.java
+++ 0044-wildcard-matching/0044-wildcard-matching.java
@@ -1,39 +1,31 @@
 class Solution {
-    private int my_fun(String s, int idx, String p, int idx2, int[][] dp) {
-        if (idx2 == p.length()) {
-            if (idx == s.length()) return 1;
-            return 0;
-        }
-        if (dp[idx][idx2] != -1) return dp[idx][idx2];
-        if (idx == s.length()) {
-            if (idx2 < p.length() - 1 && p.charAt(idx2) == '*' && p.charAt(idx2 + 1) == '*') {
-                dp[idx][idx2] = my_fun(s, idx, p, idx2 + 1, dp);
-            } else if (p.charAt(idx2) == '*') {
-                dp[idx][idx2] = my_fun(s, idx, p, idx2 + 1, dp);
-            } else {
-                dp[idx][idx2] = 0;
+    private int f(int i,int j,String s,String p,int[][] dp){
+        if(j >= p.length()) return  i>=s.length() ? 1 : 0;
+        else if(i>= s.length()){
+            for(int c = j ;c <p.length();c++){
+                if(p.charAt(c) != '*') return 0;
             }
-            return dp[idx][idx2];
+            return 1;
         }
-        if (p.charAt(idx2) == '*') {
-            dp[idx][idx2] = my_fun(s, idx + 1, p, idx2, dp) | my_fun(s, idx, p, idx2 + 1, dp);
-        } else if (p.charAt(idx2) == '?' || p.charAt(idx2) == s.charAt(idx)) {
-            dp[idx][idx2] = my_fun(s, idx + 1, p, idx2 + 1, dp);
-        } else {
-            dp[idx][idx2] = 0;
+        if(dp[i][j] != -1) return dp[i][j];
+        if(s.charAt(i)==p.charAt(j) || p.charAt(j)=='?'){
+            dp[i][j] = f(i+1,j+1,s,p,dp);
+            return dp[i][j];
+        }else if(p.charAt(j) == '*'){
+            dp[i][j] = f(i,j+1,s,p,dp) | f(i+1,j,s,p,dp);
+            return dp[i][j];
         }
-        return dp[idx][idx2];
+        return 0;
     }
-
     public boolean isMatch(String s, String p) {
         int n = s.length();
         int m = p.length();
-        int[][] dp = new int[n + 1][m + 1];
-        for (int i = 0; i <= n; i++) {
-            for (int j = 0; j <= m; j++) {
+        int[][] dp = new int[n+1][m+1];
+        for(int i=0;i<=n;i++){
+            for(int j=0;j<=m;j++){
                 dp[i][j] = -1;
             }
         }
-        return my_fun(s, 0, p, 0, dp) == 1;
+        return f(0,0,s,p,dp) == 1;
     }
 }
