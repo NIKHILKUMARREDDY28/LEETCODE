ad4df07b475e5dee1f0a20b5c18dcb1f39a68082
NIKHILKUMARREDDY28 <98465889+NIKHILKUMARREDDY28@users.noreply.github.com>
2023-04-22 13:52:10 +0530

    Time: 17 ms (13.29%), Space: 45.6 MB (14.81%) - LeetHub

---
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.java
@@ -0,0 +1,40 @@
+class Solution {
+    private int helper(int i,int has,int cnt ,int[] prices,int[][][] dp){
+        if(i==prices.length) return 0;
+        if(dp[i][cnt][has] != -1) return dp[i][cnt][has];
+        if(cnt > 0){
+            if(has==1){
+                int sell = prices[i] + helper(i+1,0,cnt,prices,dp);
+                int not = helper(i+1,1,cnt,prices,dp);
+                dp[i][cnt][has] = Math.max(sell,not);
+                return dp[i][cnt][has];
+            }else{
+                int buy = -prices[i] + helper(i+1,1,cnt-1,prices,dp);
+                int not = helper(i+1,0,cnt,prices,dp);
+                dp[i][cnt][has] = Math.max(buy,not);
+                return dp[i][cnt][has];
+            }
+        }else{
+            if(has==1){
+                int sell = prices[i] + helper(i+1,0,cnt,prices,dp);
+                int not = helper(i+1,1,cnt,prices,dp);
+                dp[i][cnt][has] = Math.max(sell,not);
+                return dp[i][cnt][has];
+            }else{
+                dp[i][cnt][has] = helper(i+1,0,cnt,prices,dp);
+                return dp[i][cnt][has];
+            }
+        }
+        
+    }
+    public int maxProfit(int a,int[] prices) {
+        int n = prices.length;
+        int[][][] dp= new int[n][a+1][2];
+        for(int i=0;i<n;i++){
+            for(int j=0;j<=a;j++){
+                for(int k=0;k<2;k++)dp[i][j][k] = -1;
+            }
+        }
+        return helper(0,0,a,prices,dp);
+    }
+}
